c4868fa22214d23ffd7aebbaa0340539791db43a
Mariyam <farooqmaryam508@gmail.com>
2025-07-06 06:25:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! Please entera valid password")
+alert("Welcome to js land,,,\nHappy Coding")
+alert("Welcome to js land,,,")
+alert("HelloI can JS through my web browser's console ")
+
